13861beedb1ed527063d15abcae2ee8f483a781b
XandeBlu <92054535+XandeBlu@users.noreply.github.com>
2024-02-03 19:32:49 -0300

    Criado mais classes e métodos
    Criei a opção de cadastrar lojas, clientes e fornecedores. Criando a gestão de estoque.

--- main.py
+++ main.py
@@ -8,14 +8,61 @@ Regras:
 •	Não é necessário utilizar banco de dados
 •	Criar classes com setters e getters
 '''
+#----------------------------------------------------------------
+i = 0
 class Loja:
-    def __init__(self, nomeloja):
+    def __init__(self, nomeloja,cnpjloja):
         self.nomeloja = nomeloja
+        self.cnpj = cnpjloja
 
     def cadastrar_loja(self):
         self.nomeloja = input('Digite o nome da loja: ')
+        self.cnpjloja = input('Digite o cnpj da loja: ')
 
+#------------------------------------------------------------------
+class Cliente:
+    def __init__(self, nomecliente,cnpjcliente):
+        self.nomecliente = nomecliente
+        self.cnpjcliente = cnpjcliente
+
+    def cadastrarcliente(self):
+        self.nomecliente = input('Digite o nome do cliente: ')
+        self.cnpjcliente = input('Digite qual o cnpj do cliente: ')
+#-------------------------------------------------------------------
+class Fornecedor:
+    def __init__(self, nomefornecedor, cnpjfornecedor):
+        self.nomefornecedor = nomefornecedor
+        self.cnpjfornecedor = cnpjfornecedor
+
+    def cadastrarfornecedor(self):
+        self.nomefornecedor = input('Digite o nome do fornecedor: ')
+        self.cnpjfornecedor = input('Digite o cnpj do fornecedor: ')
+#--------------------------------------------------------------------
+class Estoque:
+    def __init__(self):
+        self.itens = []
+        self.precos = []
+
+    def adicionar_ao_estoque(self):
+        item = input('Digite qual produto deseja adicionar: ')
+        preco = float(input('Digite o preço do produto: '))
+        self.itens.append(item)
+        self.precos.append(preco)
+
+    def remover_do_estoque(self):
+        item_remover = input('Digite o nome do item que deseja remover: ')
+        if item_remover in self.itens:
+            indice_item = self.itens.index(item_remover)
+            del self.itens[indice_item]
+            del self.precos[indice_item]
+            print(f"{item_remover} removido do estoque.")
+        else:
+            print(f"{item_remover} não encontrado no estoque. ")
+            print('Certifique-se de ter escrito corretamente o nome do item')
+
+#--------------------------------------------------------------------
 print('Bem-Vindo ao Sistema!')
+print('')
 print('Digite 1 para cadastrar loja')
 print('Digite 2 para cadastrar clientes')
 print('Digite 3 para cadastrar fornecedores')
@@ -23,8 +70,23 @@ print('Digite 4 para acessar o estoque')
 resposta = int(input())
 
 if resposta == 1:
-    nova_loja = Loja(nomeloja="")
+    nova_loja = Loja(nomeloja="", cnpjloja="")
     nova_loja.cadastrar_loja()
     print('Loja cadastrada com sucesso!')
-else:
-    print('Opção inválida')
+
+elif resposta == 2 :
+        novo_cliente = Cliente(nomecliente="", cnpjcliente="")
+        novo_cliente.cadastrarcliente()
+        print('Cliente cadastrado com sucesso!')
+
+elif resposta == 3:
+        novo_fornecedor = Fornecedor(nomefornecedor="", cnpjfornecedor="")
+        novo_fornecedor.cadastrarfornecedor()
+        print('Fornedor cadastrado com sucesso!')
+
+elif resposta == 4:
+    print('Bem vindo ao estoque!')
+    print('1-Adicionar itens ao estoque')
+    print('2-Remover itens do estoque')
+    print('3-Ver estoque ')
+    respostaestoque = int(input())
